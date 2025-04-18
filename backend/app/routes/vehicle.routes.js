011bf4199079bfc28725f6b801d91a19da648cf0
Mac <thesmartmotion@gmail.com>
2025-04-18 18:33:51 +0700

    Add sample API to get all arus

--- backend/app/routes/vehicle.routes.js
+++ backend/app/routes/vehicle.routes.js
@@ -3,26 +3,8 @@ module.exports = app => {
 
   var router = require("express").Router();
 
-  // Create a new Vehicle
-  router.post("/", Vehicles.create);
-
   // Retrieve all Vehicles
   router.get("/", Vehicles.findAll);
 
-  // Retrieve all published Vehicles
-  router.get("/published", Vehicles.findAllPublished);
-
-  // Retrieve a single Vehicle with id
-  router.get("/:id", Vehicles.findOne);
-
-  // Update a Vehicle with id
-  router.put("/:id", Vehicles.update);
-
-  // Delete a Vehicle with id
-  router.delete("/:id", Vehicles.delete);
-
-  // Delete all Vehicles
-  router.delete("/", Vehicles.deleteAll);
-
-  app.use('/api/Vehicles', router);
+  app.use('/api/vehicles', router);
 };
