23276293fa9a46745b76b8295dc20e1cef19449d
Nadifdzaikra <nadifdzaikra@gmail.com>
2025-06-09 14:27:16 +0700

    update chart ekuivalensi update data from json

--- frontend/src/app/survei-kinerja/page.js
+++ frontend/src/app/survei-kinerja/page.js
@@ -1,19 +1,31 @@
 "use client"
 import { lazy, Suspense, useEffect, useState } from "react"
+import DataEkuivalensi from '@/data/DataEkuivalensi.json';
 
 const  SimpangTrafficKinerja = lazy (() => import("@/app/components/simpangTrafficKinerja"));
 const TrafficKinerjaTable = lazy(() => import("@/app/components/trafficSurveyTable"));
 const EkuivalensiChart = lazy(() => import("@/app/components/ekuivalensiChart"))
-const KinerjaPage = () => {
 
+const KinerjaPage = () => {
+  const [dataChart, setDataChart] = useState([]);
+  const [dataTerlindung, setDataTerlindung] = useState([]);
+  const [dataTerlawan, setDataTerlawan] = useState([]);
+  
+  useEffect(() => {
+    import('@/data/DataEkuivalensi.json').then((data) => {
+      setDataChart(data.default)
+      setDataTerlindung(data.default.filter((item) => item.type === "terlindungi" ? data.default.data : []))
+      setDataTerlawan(data.default.filter((item) => item.type === "terlawanan" ? data.default.data : []))
+    } )
+  }, [])
   return (
     <div>
       <Suspense fallback={<div className="my-5 w-full text-center">Loading...</div>}>
         <TrafficKinerjaTable />
         <SimpangTrafficKinerja />
         <div className="lg:flex w-full">
-          <EkuivalensiChart />
-          <EkuivalensiChart />
+          <EkuivalensiChart data={dataTerlindung}/>
+          <EkuivalensiChart data={dataTerlawan}/>
         </div>
       </Suspense>
     </div>
