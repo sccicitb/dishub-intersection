ff955d665eb882acd33c901ba6e4f07069e9bfa8
Nadifdzaikra <nadifdzaikra@gmail.com>
2025-06-17 21:36:17 +0700

    feat: add page mobility for smart-mobility access dashboard

--- frontend/src/app/layoutMobility.js
+++ frontend/src/app/layoutMobility.js
@@ -4,7 +4,6 @@ import * as Icons from "react-icons/ai";
 import * as FaIcons from "react-icons/fa";
 import * as RiIcons from "react-icons/ri";
 import * as MdIcons from "react-icons/md";
-import listMenu from "@/data/menu.json";
 import listMenuMobility from "@/data/menuMobility.json";
 import ThemeToggle from "@/app/components/customTheme";
 import { Logout } from "@/app/auth/logout";
@@ -19,17 +18,11 @@ const Layout = ({ children }) => {
   const { token, pathname, idUser } = useAuth();
   const [mounted, setMounted] = useState(false);
   const [loaded, setLoaded] = useState(false);
-  const [menu, setMenu] = useState([]);
 
   useEffect(() => {
     setMounted(true);
     // console.log(idUser)
   }, []);
-
-  useEffect(() => {
-    setMenu(pathname === "/dashboard/mobility" ? listMenuMobility : listMenu)
-    console.log("path")
-  }, [pathname])
 
   if (!mounted) return null;
 
@@ -49,18 +42,14 @@ const Layout = ({ children }) => {
                 <Icons.AiOutlineMenu className="inline-block text-xl" />
               </label>
             </div>
-            {pathname !== "/dashboard/mobility" && (
-              <div className="lg:mx-2 flex-1 lg:px-2 font-semibold w-fit xl:w-full xl:justify-center flex not-xl:overflow-hidden not-xl:text-ellipsis">
-                <Breadcrumbs />
+            <div className="lg:mx-2 flex-1 lg:px-2 font-semibold w-fit xl:w-full xl:justify-center flex not-xl:overflow-hidden not-xl:text-ellipsis">
+            <Breadcrumbs/>
+            </div>
+            {/* <div className="flex-none">
+              <div className="flex menu-horizontal items-center gap-2">
+                <ProfileDropdown />
               </div>
-            )}
-            {pathname !== "/dashboard/mobility" && (
-              <div className="flex-none">
-                <div className="flex menu-horizontal items-center gap-2">
-                  <ProfileDropdown />
-                </div>
-              </div>
-            )}
+            </div> */}
           </div>
           <div className="flex-1 overflow-auto">
             <PageWrapper>
@@ -72,23 +61,23 @@ const Layout = ({ children }) => {
         <div className="drawer-side z-[40]">
           <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
           <div className="menu bg-[#232f61]/90 not-2xl:rounded-tr-4xl text-base-content font-semibold min-h-full p-0 w-52 gap-1">
-            <div className="flex justify-center">
-              <div className="rounded-tr-3xl bg-neutral-800/90 w-full h-16 p-4">
-                <Image
-                  src="/image/IC_SMART MOBILITY.png"
-                  alt="Logo"
-                  width={120}
-                  height={120}
-                  className={`transition-opacity duration-500 ${loaded ? "opacity-100" : "opacity-0"}`}
-                  onLoadingComplete={() => setLoaded(true)}
-                />
-              </div>
-            </div>
-            <ul className="flex flex-col gap-2 p-2 text-[13px]">
-              {menu?.map((item, index) => {
-                // Try to find the icon in the primary Icons library
+          <div className="flex justify-center">
+          <div className="rounded-tr-3xl bg-neutral-800/90 w-full h-16 p-4">
+            <Image
+              src="/image/IC_SMART MOBILITY.png"
+              alt="Logo"
+              width={120}
+              height={120}
+              className={`transition-opacity duration-500 ${loaded ? "opacity-100" : "opacity-0"}`}
+              onLoadingComplete={() => setLoaded(true)}
+            />
+          </div>
+          </div>
+          <ul className="flex flex-col gap-2 p-2 text-[13px]">
+            {listMenuMobility?.map((item, index) => {
+              // Try to find the icon in the primary Icons library
                 let IconComponent = item.icon && Icons[item.icon];
-
+                
                 // If not found in the primary library, try the FaIcons library
                 if (!IconComponent && item.icon && FaIcons[item.icon]) {
                   IconComponent = FaIcons[item.icon];
@@ -98,15 +87,15 @@ const Layout = ({ children }) => {
                   IconComponent = RiIcons[item.icon]
                 }
                 return (
-                  <li key={index} >
-                    <a href={item.url} className={pathname === item.url ? `items-center bg-neutral-800/90 box-shadow rounded-xl text-white py-1.5` : `rounded-xl py-1.5 ` + `my-0.5 text-white`}>
-                      {IconComponent && <IconComponent className="inline-block mr-2 text-lg" />}
-                      {item.name}
-                    </a>
-                  </li>
-                );
-              })}
-              {/* <div className="lg:hidden absolute bottom-5">
+                <li key={index} >
+                  <a href={item.url} className={pathname === item.url ? `items-center bg-neutral-800/90 box-shadow rounded-xl text-white py-1.5` : `rounded-xl py-1.5 ` + `my-0.5 text-white`}>
+                    {IconComponent && <IconComponent className="inline-block mr-2 text-lg" />}
+                    {item.name}
+                  </a>
+                </li>
+              );
+            })}
+            {/* <div className="lg:hidden absolute bottom-5">
               <div className="flex flex-col items-start gap-2">
               <div className="flex">
                 <ProfileDropdown />
@@ -115,7 +104,7 @@ const Layout = ({ children }) => {
               </div>
               </div>
             </div> */}
-            </ul>
+          </ul>
           </div>
         </div>
       </div>
